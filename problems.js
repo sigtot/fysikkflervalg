ba2e9a0f0a539aeb2f07d65e2a7e9dad65bccdcb
sigtot <sigurdtotland@gmail.com>
2017-11-20 16:51:30 +0100

    Initial structure

---
+++ problems.js
@@ -0,0 +1,19 @@
+const A = 0;
+const B = 1;
+const C = 2;
+const D = 3;
+const E = 4;
+
+const alternatives = ["A", "B", "C", "D", "E"];
+
+const FALL_2006 = "Høst 2006";
+
+var problems = [
+    {
+        problemFileName: "2006_1.png",
+        answerFileName: "2006_1_LF.png",
+        originExam: FALL_2006,
+        answer: D,
+        numberOfAlternatives: 5,
+    }
+];
