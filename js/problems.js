64ce3c2c74d552ee0f77e718ed3c9e1c8d4b2500
sigtot <sigurdtotland@gmail.com>
2017-11-21 18:04:07 +0100

    Renamed some image stuff

--- js/problems.js
+++ js/problems.js
@@ -25,9 +25,9 @@ const tfy4115 = new Subject("Fysikk", "TFY4115");
 const subjects = [mekfys, tfy4115];
 
 // Exams
-const FALL_2006 = new Exam("Høst 2006", new Date("2006-12-08"), mekfys);
+const MEKFYS_FALL_2006 = new Exam("Høst 2006", new Date("2006-12-08"), mekfys);
 
-const exams = [FALL_2006];
+const exams = [MEKFYS_FALL_2006];
 
 // Categories
 const mekanikk = "Mekanikk";
@@ -35,10 +35,18 @@ const termo = "Termodynamikk";
 
 var problems = [
     {
-        problemFileName: "2006_1.png",
-        answerFileName: "2006_1_LF.png",
-        originExam: FALL_2006,
+        problemFileName: "mekfys_2006_1.png",
+        answerFileName: "mekfys_2006_1_LF.png",
+        originExam: MEKFYS_FALL_2006,
         answer: D,
+        numberOfAlternatives: 5,
+        category: mekanikk,
+    },
+    {
+        problemFileName: "mekfys_2006_1.png",
+        answerFileName: "mekfys_2006_1_LF.png",
+        originExam: MEKFYS_FALL_2006,
+        answer: B,
         numberOfAlternatives: 5,
         category: mekanikk,
     },
