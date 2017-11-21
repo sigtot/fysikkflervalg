d38eed65649a4fcfe4555009026d0ec635a8cec5
sigtot <sigurdtotland@gmail.com>
2017-11-21 17:52:26 +0100

    Answer stuff

--- js/dom.js
+++ js/dom.js
@@ -5,8 +5,10 @@ var numberOfProblemsInput = $("#number-of-problems-input");
 var imageContainer = $("#image-container");
 var alternativesContainer = $("#alternatives-container");
 var infoContainer = $("#info-container");
+var answerViewer = $("#answer-viewer");
 var sessionSettingsElement = $("#session-settings");
 var problemViewer = $("#problem-viewer");
+var alternativeButtons = [];
 
 numberOfProblemsInput.val(Math.min(defaultSessionSize,problems.length));
 numberOfProblemsInput.attr("max", problems.length);
@@ -35,14 +37,20 @@ function hideProblemViewer(){
     problemViewer.css("display", "none");
 }
 
+function hideAnswerViewer(){
+    answerViewer.css("display", "none");
+}
+
 function displayProblem(problem){
     imageContainer.empty();
     alternativesContainer.empty();
     infoContainer.empty();
 
+    // Display image
     var image = $("<img>").attr("src", imageFolderPath + problem.problemFileName);
     imageContainer.append(image);
 
+    // Display alternatives
     for(var i = 0; i < problem.numberOfAlternatives; i++){
         var alternativeButton = $("<button></button>");
         alternativeButton.attr("class", "btn btn-primary btn-lg medium-margins");
@@ -50,14 +58,38 @@ function displayProblem(problem){
         alternativeButton.data("alternative", i);
         alternativeButton.click(function(){
             var thisButton = $(this);
+
             var isCorrectAnswer = session.submitAnswer(thisButton.data("alternative"));
-            if(isCorrectAnswer){
-                thisButton.addClass("correct-answer-button");
-            } else {
-                thisButton.addClass("wrong-answer-button");
+
+            if(!isCorrectAnswer){
+                alternativeButtons[thisButton.data("alternative")].addClass("wrong-answer-button");
             }
+                alternativeButtons[problem.answer].addClass("correct-answer-button");
+
+            // Answer needs to be submitted. We can use isCorrectAnswer to add crazy congratulatory effects later maybe
+            displayAnswer(problem);
         });
         alternativesContainer.append(alternativeButton);
+        alternativeButtons.push(alternativeButton);
     }
 }
 
+function displayAnswer(problem){
+    answerViewer.css("display", "block");
+    answerViewer.empty();
+
+    var image = $("<img>").attr("src", imageFolderPath + problem.answerFileName);
+    answerViewer.append(image);
+
+    answerViewer.append($("<br>"));
+
+    var button = $("<button></button>");
+    button.attr("class", "btn btn-primary btn-lg medium-margins");
+    if(session.isFinished()){
+        button.text("All done :)");
+        button.addClass("disabled");
+    } else {
+        button.text("Neste oppgave");
+    }
+    answerViewer.append(button);
+}
